3a038056750ceb9574db8ad45e367775afa38a40
ryandavid-oss <ryandavid@me.com>
2026-02-26 14:49:36 -0700

    Update wayback_purified.js

--- wayback_purified.js
+++ wayback_purified.js
@@ -1,27 +1,6 @@
 const wayback_raw_dump = [
 
     {
-        "file": "2026-02-24-a-new-otw.md",
-        "year": "2026",
-        "date": "### February 24, 2026",
-        "title": "A new OTW",
-        "body": "---\n\nWhere have I heard that before?  Surely, at least five times on this site during the past 26 years.    \n  \n**26 years, btw.**   That's how long I've had this domain registered and operating in one form or another.  I finally felt the drive to really sit down and get all of the accumulated digital cruft organized and back online.  So, what you see is the first stage of the process.  I still haven't quite decided whether I will keep the site divided into Personal/Professional ongoing.  But at this point in time, the \"Personal\" site is mostly online and functional.  \n  \nWith the personal side, you'll find a trove of old data on the right side of the screen, separated into \"cards\". The ones most interesting for visitors will likely be [The Archive] and [The Flotsam], for that is where most of the past has been resurrected and then buried again.    \n  \nYou'll also see a bit of boutique linkage in the form of a limited edition run of OTW t-shirts.  These are pretty fabulous unisex shirts that all your friends will pine over, surely.  Why not grab one?  \n  \nThere is certainly more work to be done over the comings weeks, so stay tuned.  At the very least, the OTW blog will be updated regularly again, so there's that.  \n  \nThanks for visiting!"
-    },
-    {
-        "file": "2026-02-20-maybe--a-poem-.md",
-        "year": "2026",
-        "date": "### February 20, 2026",
-        "title": "Maybe, a poem?",
-        "body": "---\n\nFor as far as you wish    \nFor as long as you stretch    \nIn the moment you lumber    \nIn haste, toward the mountain    \n    \nThe general truth    \nThe uncourteous example    \nNever minding the canopy    \nStretching, aching with heaviness  \n  \nReminder, your cohort  \nBelieves in the coddling fervor  \nThe mind-numbing ache  \nThe key-turn freeing  \n  \nWhat's the angle, then?  \nTo a friend, just perspiration  \nTo our valley below, it's normality  \nDo you remember the promise?  \n  \nSo take wing to the summit  \nThree paths, picked not yet  \nWe may celebrate not the gift  \nAnd yet, burn pleasant all the same"
-    },
-    {
-        "file": "2026-02-19-it-s-a-straight-vibe.md",
-        "year": "2026",
-        "date": "### February 19, 2026",
-        "title": "It's a straight VIBE",
-        "body": "---\n\nHallelujah, folks. We are back online and fully ready to rock this site back into existence.  \n  \nFor the longest time, I fell back on 3rd party tools and services to help post thoughts to the ether. This was my mechanism for years after I fell into silence while my life took the crazy train without me (with me?) as a passenger. But I digress.  \n  \nBack in 2000, my original site was near pure HTML, with each page created lovingly within Microsoft FrontPage (remember that?) I only once or twice \"borrowed\" code from sting.com, snowbird.com, etc...  \n  \nSince we're partying like it's 1999, OTW is back to pure code, but instead of learning from scratch, I am using Gemini to \"vibe\" with me as I tell it what I want and where I want it. I even created a native posting tool so that I can leave Blogger behind.  \n  \nSo stay tuned,  more magic is guaranteed to manifest itself as the days go on..."
-    },
-    {
         "file": "2025-05-21-Weight--inexplicable.md",
         "year": "2025",
         "date": "May 21, 2025",
