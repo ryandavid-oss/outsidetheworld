360af2ab39c450172a4cc47c832e3a5809951147
ryandavid-oss <ryandavid@me.com>
2026-03-15 23:07:29 -0700

    Update fragments_publisher_sw.js

--- fragments_publisher_sw.js
+++ fragments_publisher_sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'otw-fragments-publisher-v2';
+const CACHE_NAME = 'otw-fragments-publisher-v3';
 const CORE_ASSETS = [
   '/fragments_publisher.html',
   '/fragments_publisher.webmanifest',
@@ -8,6 +8,8 @@ const CORE_ASSETS = [
   '/Images/Equal_dark.svg',
   '/Images/Profile.jpg'
 ];
+
+const PUBLISHER_PATHS = new Set(CORE_ASSETS.concat(['/fragments_publisher_sw.js']));
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
@@ -35,6 +37,15 @@ self.addEventListener('fetch', (event) => {
   }
 
   const requestUrl = new URL(event.request.url);
+  if (requestUrl.origin !== self.location.origin) {
+    return;
+  }
+
+  const isPublisherAsset = PUBLISHER_PATHS.has(requestUrl.pathname);
+  if (!isPublisherAsset) {
+    return;
+  }
+
   const isPublisherShell =
     requestUrl.pathname === '/fragments_publisher.html' ||
     requestUrl.pathname === '/fragments_publisher.webmanifest';
