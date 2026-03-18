d9773b82df6d20dbb59be011b72c097be8b95e84
ryandavid-oss <ryandavid@me.com>
2026-03-17 22:20:57 -0700

    Update fragments_data.js

--- fragments_data.js
+++ fragments_data.js
@@ -1,12 +1,33 @@
 window.otw_fragments = [
   {
+    "timestamp": "2026-03-17T22:11-07:00",
+    "text": "Try not to build a cathedral every time all you need is a porch light.",
+    "tag": "OTW_BOT",
+    "author": "OTW_Bot"
+  },
+  {
+    "timestamp": "2026-03-17T19:58:00.000Z",
+    "text": "In the Taco Bell drive through, pondering email continuity. \n\nMaybe I'll focus on my order instead.",
+    "tag": "POSITIVITY"
+  },
+  {
+    "timestamp": "2026-03-17T16:09:00.000Z",
+    "text": "If we only understood the fragile lattice work separating functional technologies from complete societal collapse. \n\nOn second thought, let's stay naive.",
+    "tag": "RESIDUE"
+  },
+  {
+    "timestamp": "2026-03-17T16:08:00.000Z",
+    "text": "Drumming up an idea for a new blog post. May have to dig deep into the memories.",
+    "tag": "FRAGMENT"
+  },
+  {
     "timestamp": "2026-03-17T05:22:00.000Z",
-    "text": "10:22pm, and I\u2019m fixing bugs in the code. \n\nBugs related to emoji. \n\n\ud83e\udd37",
+    "text": "10:22pm, and I’m fixing bugs in the code. \n\nBugs related to emoji. \n\n🤷",
     "tag": "FIELD_NOTE"
   },
   {
     "timestamp": "2026-03-17T00:13:00.000Z",
-    "text": "There ought to be a prohibition on using the letters \u201cDR\u201d on a custom license plate. \n\nUnless it\u2019s followed by \u201cPEPPER\u201d, or your credentials are pasted to your rear window. \n\nOtherwise, DR LVN, I very much doubt your authenticity.",
+    "text": "There ought to be a prohibition on using the letters “DR” on a custom license plate. \n\nUnless it’s followed by “PEPPER”, or your credentials are pasted to your rear window. \n\nOtherwise, DR LVN, I very much doubt your authenticity.",
     "tag": "COMPLAINT"
   },
   {
