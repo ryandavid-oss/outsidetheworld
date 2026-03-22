bb868ccd7d1016053922b11032df032ba3d28bb9
ryandavid-oss <ryandavid@me.com>
2026-03-21 20:28:43 -0700

    Publish fragment

--- fragments_data.js
+++ fragments_data.js
@@ -8,6 +8,14 @@ window.otw_fragments = [
     "author_handle": "@sadie"
   },
   {
+    "timestamp": "2026-03-22T03:28:39.774Z",
+    "text": "My dad is so smart and his website is really inspiring and he’s the best.",
+    "tag": "FRAGMENT",
+    "author_id": "sadie",
+    "author": "Sadie",
+    "author_handle": "@sadie"
+  },
+  {
     "timestamp": "2026-03-22T03:28:38.868Z",
     "text": "My dad is so smart and his website is really inspiring and he’s the best.",
     "tag": "FRAGMENT",
