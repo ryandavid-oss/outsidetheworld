3ad95da055b0726cabf38c49f091fcc1a1814296
ryandavid-oss <ryandavid@me.com>
2026-03-23 14:09:33 -0700

    Publish fragment

--- fragments_data.js
+++ fragments_data.js
@@ -1,5 +1,13 @@
 window.otw_fragments = [
   {
+    "timestamp": "2026-03-23T21:09:32.200Z",
+    "text": "An older man told me at the store today that he liked my outfit. He said that it was classy and really nice, but also comfortable looking. Then he said, “Wow, I’m slacking!” It was cute! Love it when people give sweet compliments and they’re not trying to hit on you!",
+    "tag": "FRAGMENT",
+    "author_id": "emily",
+    "author": "Emily",
+    "author_handle": "@word.person"
+  },
+  {
     "timestamp": "2026-03-23T17:54:50.027Z",
     "text": "You are allowed to keep the post small and the feeling true.",
     "tag": "OTW_BOT",
