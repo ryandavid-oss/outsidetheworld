c9590c3e213f4be9eaeeea52de1521c9bffeb6fb
ryandavid-oss <ryandavid@me.com>
2026-03-28 08:36:35 -0700

    Publish fragment

--- fragments_data.js
+++ fragments_data.js
@@ -1,5 +1,13 @@
 window.otw_fragments = [
   {
+    "timestamp": "2026-03-28T15:36:00.000Z",
+    "text": "It has a pulse.",
+    "tag": "FRAGMENT",
+    "author_id": "ryan",
+    "author": "The_RyanDavid",
+    "author_handle": "@outsidetheworld"
+  },
+  {
     "timestamp": "2026-03-28T06:07:00.000Z",
     "text": "Let's see if we can bring this dream to life, shall we?",
     "tag": "POSITIVITY",
